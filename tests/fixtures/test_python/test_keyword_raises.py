412821bb443d1c46ea8b2cd914c6e168f10719c6
detachhead <detachhead@users.noreply.github.com>
2023-09-15 14:36:14 +1000

    fix some issues with context manager keywords

---
+++ tests/fixtures/test_python/test_keyword_raises.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from robot.api import logger
+
+from pytest_robotframework import keyword
+
+
+class FooError(Exception):
+    pass
+
+
+@keyword
+def bar():
+    raise FooError
+
+
+def test_foo():
+    bar()
+    logger.info(1)  # type:ignore[no-untyped-call]
