3518f68fadc46d6826d2eba896257f73e60bb91e
DetachHead <detachhead@users.noreply.github.com>
2023-09-20 20:56:10 +1000

    fis `catch_errors` decorator breaking static methods

---
+++ tests/fixtures/test_python/test_catch_errors_decorator_with_non_instance_method/conftest.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from robot.api.interfaces import ListenerV3
+from typing_extensions import override
+
+from pytest_robotframework import listener
+
+if TYPE_CHECKING:
+    from robot import model, result
+
+ran = False
+
+
+@listener
+class Listener(ListenerV3):
+    @staticmethod
+    def static_method():
+        pass
+
+    @classmethod
+    def class_method(cls):
+        pass
+
+    @override
+    def start_test(self, data: model.TestCase, result: result.TestCase):
+        self.static_method()
+        self.class_method()
