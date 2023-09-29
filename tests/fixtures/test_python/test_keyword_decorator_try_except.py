192c15a503cbb4ff1728f5ebda573b7ea2c8be24
DetachHead <detachhead@users.noreply.github.com>
2023-09-29 14:19:44 +1000

    deprecate `continue_on_error` in favor of try/except statements

---
+++ tests/fixtures/test_python/test_keyword_decorator_try_except.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from robot.api import logger
+
+from pytest_robotframework import keyword
+
+
+class FooError(Exception):
+    pass
+
+
+@keyword
+def bar():
+    raise FooError
+
+
+def test_foo():
+    try:
+        bar()
+    except FooError:
+        logger.info("hi")  # type:ignore[no-untyped-call]
