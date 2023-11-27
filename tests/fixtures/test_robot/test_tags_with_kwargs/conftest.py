b741bf38e35d8420297b31eebd4c3434ee80859c
Loch64 <58125882+Loch64@users.noreply.github.com>
2023-11-27 15:46:22 +1000

    simplified adding and testing marker kwargs

--- tests/fixtures/test_robot/test_tags_with_kwargs/conftest.py
+++ tests/fixtures/test_robot/test_tags_with_kwargs/conftest.py
@@ -5,6 +5,6 @@ def pytest_runtest_setup(item: RobotItem):
     assert item.name == "Foo"
     marker_names = ["m1", "m2"]
     marker_kwargs = [{"foo": "1", "baz": "false"}, {"bar": "foo", "qux": "7"}]
-    for i, marker in enumerate(item.iter_markers()):
-        assert marker.name == marker_names[i]
-        assert marker.kwargs == marker_kwargs[i]  # type: ignore[no-any-expr]
+    for marker, marker_name, marker_kwarg in zip(item.iter_markers(), marker_names, marker_kwargs, strict=True):
+        assert marker.name == marker_name
+        assert marker.kwargs == marker_kwarg  # type: ignore[no-any-expr]
