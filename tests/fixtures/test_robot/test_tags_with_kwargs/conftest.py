9c8784fa58ce140c7bb988cf5522ff9f2494c1c0
loch64 <loch64@users.noreply.github.com>
2023-11-27 16:00:39 +1000

    added init file, assert for marker kwargs test

--- tests/fixtures/test_robot/test_tags_with_kwargs/conftest.py
+++ tests/fixtures/test_robot/test_tags_with_kwargs/conftest.py
@@ -1,10 +1,18 @@
-from pytest_robotframework._internal.pytest_robot_items import RobotItem
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from pytest_robotframework._internal.pytest_robot_items import RobotItem
 
 
 def pytest_runtest_setup(item: RobotItem):
     assert item.name == "Foo"
     marker_names = ["m1", "m2"]
     marker_kwargs = [{"foo": "1", "baz": "false"}, {"bar": "foo", "qux": "7"}]
-    for marker, marker_name, marker_kwarg in zip(item.iter_markers(), marker_names, marker_kwargs, strict=True):
+    markers = list(item.iter_markers())
+    # TODO: use strict=True instead when dropping support for <3.10 # noqa: TD003
+    assert len(marker_names) == len(marker_kwargs) == len(markers)
+    for marker, marker_name, marker_kwarg in zip(markers, marker_names, marker_kwargs):
         assert marker.name == marker_name
-        assert marker.kwargs == marker_kwarg  # type: ignore[no-any-expr]
+        assert marker.kwargs == marker_kwarg  # type:ignore[no-any-expr]
