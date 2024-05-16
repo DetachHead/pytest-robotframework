0259f4b397098462cc93445e8b946654dae9782e
detachhead <detachhead@users.noreply.github.com>
2024-05-16 13:01:31 +1000

    fix crash caused by xdist workers not running the correct tests in some cases

---
+++ tests/fixtures/test_python/test_class_three_tests_one_fail.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+
+class TestFoo:
+    @staticmethod
+    def test_one():
+        pass
+
+    @staticmethod
+    def test_two():
+        raise Exception("asdf")
+
+    @staticmethod
+    def test_three():
+        pass
